5cbdc2b6895d0ebdff58b333e2ca358fabaab52a
Saron <saronabebe2019@gmail.com>
2025-04-22 19:09:28 -0800

    Add CSS to the skills container

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let skills_container = document.querySelector(".skills");
+
+let skills = document.querySelectorAll(".skills ul div li");
+let progressRates = document.querySelectorAll(".subcontent span");
+let progressBars = document.querySelectorAll(".progress-bar .progress");
+
+progressBarFilling();
+
+function progressBarFilling() {
+  progressBars.forEach((bar, index) => {
+    const percent = progressRates[index].textContent.trim();
+    bar.style.width = percent;
+    bar.classList.add("active");
+  });
+}
